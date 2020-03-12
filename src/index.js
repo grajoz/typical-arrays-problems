454c8b4481ad6ae4c10c8749722e079fe0aefdc8
grajoz <grajoz@yandex.ru>
2020-03-12 08:43:47 +0400

    feat: typi....-arr..-pro..,,,

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array)) {return 0};
+  if (array.length == 0) { return 0 };
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+  let min=array.sort(compareNumeric)[0];
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+if (!Array.isArray(array)) {return 0};
+  if (array.length == 0) { return 0 };
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+  let max=array.sort(compareNumeric).reverse()[0];
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array)) {return 0};
+  if (array.length==0) { return 0 };
+avg=0;
+  for (i=0;i<array.length;i++){
+    array[i] = Number(array[i]);
+    avg=avg+array[i]
+  }
+  avg=avg/(array.length);
+  return avg;
 }
+
